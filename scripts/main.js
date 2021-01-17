178ae6463c4b3fd0271e91622ff5af5826de22cd
phedrakeson <gabrield.developer@gmail.com>
2021-01-17 15:17:20 -0300

    implementando funcao da loading screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 import WeatherService from './modules/weather.js';
+import LoadingScreen from './modules/loading-screen.js'
 
 const weather = new WeatherService('.search', '.search input');
+const loadingScreen = new LoadingScreen('.loading-screen');
 
 weather.init();
+loadingScreen.init();
