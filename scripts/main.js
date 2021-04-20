b279f8c446c06eb3dd34cfc2cb433525c17d983a
Lucas Torres <casteluc.dev@gmail.com>
2021-04-20 03:22:46 -0300

    feat: create dynamic input datalist

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ import LoadingScreen from './modules/loading-screen.js'
 import DynamicBackground from './modules/dynamic-background.js';
 import CitiesAutocomplete from './modules/cities-autocomplete.js'
 
-const citiesAutocomplete = new CitiesAutocomplete('.search input')
+const citiesAutocomplete = new CitiesAutocomplete('.search input', 'datalist')
 const weather = new WeatherService('.search', '.search input');
 const loadingScreen = new LoadingScreen('.loading-screen');
 const dynamicBackground = new DynamicBackground('body');
--- scripts/modules/cities-autocomplete.js
+++ scripts/modules/cities-autocomplete.js
@@ -1,23 +1,42 @@
 export default class AutocompleteCities {
-    constructor(input) {
+    constructor(input, datalist) {
         this.input = document.querySelector(input);
+        this.citiesDatalist = document.querySelector(datalist)
         this.getAllBrazilianCities = this.getAllBrazilianCities.bind(this)
+        this.createDataList = this.createDataList.bind(this);
+        this.loadOptionsOnInputChange = this.loadOptionsOnInputChange.bind(this);
         this.cities = [];
     }
   
-    autocomplete() {
-        console.log("autocomplete function");
+    createDataList() {
+        this.cities.map(city => {
+            let newCityOption = document.createElement('option');
+            newCityOption.value = city;
+            this.citiesDatalist.appendChild(newCityOption);
+        })
+    }
+
+    loadOptionsOnInputChange() {
+        this.input.addEventListener('input', e => {
+            console.log("entrou")
+            e.target.value ?
+                e.target.setAttribute('list', 'brazilian-cities-data')
+                :
+                e.target.setAttribute('list', '')
+        })
     }
 
     async getAllBrazilianCities() {
         const res = await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/municipios");
         const resJSON = await res.json();
         resJSON.map(city => {
-            this.cities.push(`${city.nome} - ${city.microrregiao.mesorregiao.UF.sigla}`)
+            this.cities.push(`${city.nome}`);
         })
+        this.createDataList();
     }
 
     init() {
+        this.loadOptionsOnInputChange();
         this.getAllBrazilianCities();
     }
 }
